58b3d0e78215d849c0a7a1b151bee829f2072117
Tsukani <34612305+Tsukani@users.noreply.github.com>
2020-09-15 22:44:39 +0200

    Fixed relog

--- index.js
+++ index.js
@@ -77,6 +77,11 @@ function clock(sessionID, cookies) {
                 }
             } else {
                 console.log("\x1b[31m%s\x1b[0m", `[${time}] Failed to load showcase status. Status code: ${response.statusCode}`);
+                if (steamResponse.statusCode == 302) {
+                  console.log(colors.yellow, `Steam session has expired. Relogging...`);
+                  clearInterval(updateInterval);
+                  return client.relog();
+                }
             }
         });
     } catch(e){console.log("\x1b[36m%s\x1b[0m", `An error occured: ${e}`);}
